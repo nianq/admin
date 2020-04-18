9523cb2d2fe0c4b4a7198679f45a59ffa176d1bf
niaq <13307493973@163.com>
2020-04-19 00:09:03 +0800

    tj

--- guli-admin/src/views/Home.vue
+++ guli-admin/src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <el-container style="height: 500px; border: 1px solid #eee">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+  <el-header class="homeHeader">
+                <div class="title">哇哈哈</div>
+                <div>
+                    <el-button icon="el-icon-bell" type="text" style="margin-right: 8px;color: #000000;" size="normal"></el-button>
+                    <el-dropdown class="userInfo">
+
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item command="userinfo">个人中心</el-dropdown-item>
+                            <el-dropdown-item command="setting">设置</el-dropdown-item>
+                            <el-dropdown-item command="logout" divided>注销登录</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </div>
+            </el-header>
+    <el-menu>
+
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-message"></i>导航一</template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
+  
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+    
+    <el-main>
+      <h1>首页</h1>
+    </el-main>
+  </el-container>
+</el-container>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "Home",
+       data() {
+        return {
+        }
+      },
+        computed: {
+          
+        },
+        methods: {
+        }
+    }
 </script>
+
+<style>
+   .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+</style>
---
+++ guli-admin/src/views/Login.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <el-form
+                class="loginContainer">
+            <h3 class="loginTitle">系统登录</h3>
+            <el-form-item>
+                <el-input size="normal" type="text" auto-complete="off" v-model="loginForm.username"
+                          placeholder="请输入用户名"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+                <el-input size="normal" type="password"  auto-complete="off" v-model="loginForm.password"
+                          placeholder="请输入密码"></el-input>
+            </el-form-item>
+            
+            <el-button size="normal" type="primary" style="width: 100%;" @click="submitLogin">登录</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Login",
+        data() {
+            return {
+                loading: false,
+                loginForm: {
+                    username: 'admin',
+                    password: '123',
+                },
+               
+            }
+        },
+        methods: {
+          
+            submitLogin() {
+                
+               let path = this.$route.query.redirect;
+               this.$router.replace((path == '/' || path == undefined) ? '/home' : path);
+                          
+                }
+            }
+        
+    }
+</script>
+
+<style>
+    .loginContainer {
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding: 15px 35px 15px 35px;
+        background: #fff;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+
+    .loginTitle {
+        margin: 15px auto 20px auto;
+        text-align: center;
+        color: #505458;
+    }
+
+    .loginRemember {
+        text-align: left;
+        margin: 0px 0px 15px 0px;
+    }
+    .el-form-item__content{
+        display: flex;
+        align-items: center;
+    }
+</style>
