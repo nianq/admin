a7f727231a4bf0e53315cb9b38722ccab1c31dcb
niaq <13307493973@163.com>
2020-04-22 22:11:54 +0800

    优化

--- guli-admin/src/views/edu/teacher/list.vue
+++ guli-admin/src/views/edu/teacher/list.vue
@@ -1,60 +1,57 @@
 <template>
   <div class="app-container">
     
-<!--查询表单-->
-    <el-form class="demo-form-inline" :model="teacherQuery">
-        <el-row :gutter="20" type="flex" class="row-bg" justify="space-around">
-        <el-col :span="8">
-          <el-form-item label="讲师名称：">
-          <el-input v-model="teacherQuery.name" placeholder="讲师名"/>
-        </el-form-item>
+  <div>
+      <!--查询表单-->
+      <el-form class="demo-form-inline" :model="teacherQuery">
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <el-form-item label="讲师名称：">
+             <el-input v-model="teacherQuery.name" placeholder="讲师名" style="width: 250px;"/>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="讲师头衔：">
+            <el-select v-model="teacherQuery.level" clearable placeholder="讲师头衔" style="width: 250px;">
+              <el-option :value="1" label="高级讲师"/>
+              <el-option :value="2" label="首席讲师"/>
+            </el-select>
+          </el-form-item>
         </el-col>
-        <el-col :span="8">
-          <el-form-item label="讲师头衔：">
-          <el-select v-model="teacherQuery.level" clearable placeholder="讲师头衔">
-            <el-option :value="1" label="高级讲师"/>
-            <el-option :value="2" label="首席讲师"/>
-          </el-select>
-      </el-form-item>
-      </el-col>
     </el-row>
-    <el-row :gutter="20" type="flex" class="row-bg" justify="space-around">
+
+    <el-row :gutter="20">
       <el-col :span="8">
-         <el-form-item label="添加时间：">
+         <el-form-item label="添加时间：" >
           <el-date-picker
-            v-model="teacherQuery.begin"
-            type="datetime"
-            placeholder="选择开始时间"
-            value-format="yyyy-MM-dd HH:mm:ss"
-            default-time="00:00:00"
-          />
+          v-model="teacherQuery.begin"
+          type="date"
+          placeholder="选择开始日期" style="width: 250px;" format="yyyy-MM-dd" value-format="yyyy-MM-dd">
+        </el-date-picker>
       </el-form-item>  
       </el-col>
       <el-col :span="8"> 
         <el-form-item label="结束时间：">
-        <el-date-picker
+          <el-date-picker
           v-model="teacherQuery.end"
-          type="datetime"
-          placeholder="选择截止时间"
-          value-format="yyyy-MM-dd HH:mm:ss"
-          default-time="00:00:00"
-        />
+          type="date"
+          placeholder="选择结束日期" style="width: 250px;" format="yyyy-MM-dd" value-format="yyyy-MM-dd">
+        </el-date-picker>
       </el-form-item>
       </el-col>
      
     </el-row>
-     
-
-      
-
-     
-     
    
       <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
       <el-button type="default" @click="resetData()">清空</el-button>
     </el-form>
-<!-- 表格 -->
+  </div>
+    <div style="margin-top: 10px;">
+         <!-- 表格 -->
     <el-table
+      v-loading="loading"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
       :data="list"
       border
       fit
@@ -62,31 +59,31 @@
 
       <el-table-column
         label="序号"
-        width="70"
         align="center">
         <template slot-scope="scope">
           {{ scope.$index + 1 }}
         </template>
       </el-table-column>
 
-      <el-table-column prop="name" label="名称" width="80" />
+        
+      <el-table-column prop="name" label="名称"   align="center"/>
 
-      <el-table-column label="头衔" width="80">
+      <el-table-column label="头衔"  align="center">
         <template slot-scope="scope">
           {{ scope.row.level===1?'高级讲师':'首席讲师' }}
         </template>
       </el-table-column>
 
-      <el-table-column prop="intro" label="资历" />
+      <el-table-column prop="intro" label="资历"  align="center"/>
 
-      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
+      <el-table-column prop="gmtCreate" label="添加时间"  align="center"/>
 
-      <el-table-column prop="sort" label="排序" width="60" />
+      <el-table-column prop="sort" label="排序"  align="center" />
 
       <el-table-column label="操作" width="200" align="center">
         <template slot-scope="scope">
             <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
-          <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="delteTeacher(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -97,6 +94,8 @@
     layout='sizes, prev, pager, next, jumper, ->, total, slot'
     :total="total"  @current-change="currentChange" @size-change="sizeChange">
   </el-pagination>
+    </div>
+   
   </div>
 </template>
 
@@ -107,6 +106,7 @@ export default {
   // },
   data() {
     return {
+      loading:true,
       list: null,
       pageSize: 6,
       currentPage: 1,
@@ -130,7 +130,8 @@ export default {
         this.pageSize +
         "/" +
         this.currentPage;
-      this.postRequest(url, this.teacherQuery).then(respones => {
+        this.postRequest(url, this.teacherQuery).then(respones => {
+        this.loading = false
         this.list = respones.data.list;
         this.total = respones.data.total;
       });
@@ -146,7 +147,30 @@ export default {
       this.getList();
     },
     //清空输入的值
-    resetData() {}
+    resetData() {
+      this.teacherQuery ={}
+      this.getList()
+    },
+    //删除讲师
+    delteTeacher(data){
+      this.$confirm('此操作将永久删除' + data.name + '讲师, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'error'
+        }).then(() => {
+         
+            this.deleteRequest('/eduservice/teacher/' + data.id).then(resp=>{
+              if(resp){
+                this.getList()
+              }
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+    }
   }
 };
 </script>
