31199e6aa3c1e5f8fb17fc2971184e90ec36a6ac
2gongjie <19908488818@163.com>
2020-04-26 17:04:20 +0800

    提交配置文件

--- guli-admin/vue.config.js
+++ guli-admin/vue.config.js
@@ -3,9 +3,18 @@ proxyObj['/ws'] = {
     ws: true,
     target: "ws://localhost:1990"
 };
-proxyObj['/'] = {
+proxyObj['/eduservice'] = {
     ws: false,
     target: 'http://localhost:1990',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/': ''
+    }
+}
+    
+proxyObj['/eduoss'] = {
+    ws: false,
+    target: 'http://localhost:1991',
     changeOrigin: true,
     pathRewrite: {
         '^/': ''
